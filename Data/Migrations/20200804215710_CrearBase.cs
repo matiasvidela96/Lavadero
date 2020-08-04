64c9680cb6bd01c6ba366084270d32a9e0e23ba3
Matias Videla <mvidela@globalis-sa.com>
2020-08-04 19:41:28 -0300

    Organizacion

---
+++ Data/Migrations/20200804215710_CrearBase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Lavadero.Data.Migrations
+{
+    public partial class CrearBase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
